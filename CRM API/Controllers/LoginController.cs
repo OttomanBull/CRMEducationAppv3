e97ecef370201e8ba891a412c1b416723fceabab
Bahadir Kayhan <bahadir.kayhan@xyzteknoloji.com>
2023-09-26 17:03:36 +0300

    Patladık (LoginController)

--- CRM API/Controllers/LoginController.cs
+++ CRM API/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Core.Entitites;
+using CRM_API.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CRM_API.Controllers
@@ -7,6 +9,47 @@ namespace CRM_API.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        private readonly RepositoryContext _context;
 
-    }
+        public LoginController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public ActionResult Login(Login login)
+        {
+            var userToLogin = _context.Login(login);
+            if (!userToLogin.Success)
+            {
+                return BadRequest(userToLogin.Message);
+            }
+
+            var result = _authService.CreateAccessToken(userToLogin.Data);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("register")]
+        public ActionResult Register(UserForRegisterDto userForRegisterDto)
+        {
+            var userExists = _authService.UserExists(userForRegisterDto.Email);
+            if (!userExists.Success)
+            {
+                return BadRequest(userExists.Message);
+            }
+
+            var registerResult = _authService.Register(userForRegisterDto, userForRegisterDto.Password);
+            var result = _authService.CreateAccessToken(registerResult.Data);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
 }
